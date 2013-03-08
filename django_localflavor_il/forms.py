7fd7faa4934466b96bd47319a767fa7097c0a257
Alonisser <alonisser@gmail.com>
2013-03-08 21:05:16 +0200

    added an israeli mobile phone number validator

--- django_localflavor_il/forms.py
+++ django_localflavor_il/forms.py
@@ -19,8 +19,10 @@ from django.utils.translation import ugettext_lazy as _
 #
 # (hebrew) http://he.wikipedia.org/wiki/%D7%9E%D7%A1%D7%A4%D7%A8_%D7%96%D7%94%D7%95%D7%AA_(%D7%99%D7%A9%D7%A8%D7%90%D7%9C)
 # (hebrew) http://he.wikipedia.org/wiki/%D7%A1%D7%A4%D7%A8%D7%AA_%D7%91%D7%99%D7%A7%D7%95%D7%A8%D7%AA
+# (hebrew) http://he.wikipedia.org/wiki/%D7%A7%D7%99%D7%93%D7%95%D7%9E%D7%AA_%D7%98%D7%9C%D7%A4%D7%95%D7%9F_%D7%91%D7%99%D7%A9%D7%A8%D7%90%D7%9C#.D7.A7.D7.99.D7.93.D7.95.D7.9E.D7.95.D7.AA_.D7.91.D7.99.D7.A9.D7.A8.D7.90.D7.9C_.D7.9C.D7.A4.D7.99_.D7.9E.D7.A4.D7.A2.D7.99.D7.9C.D7.99.D7.9D_.D7.95.D7.97.D7.9C.D7.95.D7.A7.D7.94_.D7.92.D7.90.D7.95.D7.92.D7.A8.D7.A4.D7.99.D7.AA
 
 id_number_re = re.compile(r'^(?P<number>\d{1,8})-?(?P<check>\d)$')
+mobile_phone_number = re.compile(r'^0?(5[023456789])(\-)?\d{7}$') #including palestinian mobile carriers
 
 class ILPostalCodeField(RegexField):
     """
@@ -63,5 +65,25 @@ class ILIDNumberField(Field):
 
         value = match.group('number') + match.group('check')
         if not luhn(value):
+            raise ValidationError(self.error_messages['invalid'])
+        return value
+
+class ILMobilePhoneNumber(Field):
+    """
+    A form field that validated its input as an Israeli Mobile phone number.
+    """
+
+    default_error_messages = {
+        'invalid':_('Enter a valid Mobile Number'),
+    }
+
+    def clean(self, value):
+        value = super(ILIDNumberField, self).clean(value)
+
+        if value in EMPTY_VALUES:
+            return ''
+
+        match = mobile_phone_number.match(value)
+        if not match:
             raise ValidationError(self.error_messages['invalid'])
         return value
